9e676251945073460eae57cfa2e3cd6021ccb0ca
Saira603K <114383320+Saira603K@users.noreply.github.com>
2023-04-18 21:04:19 -0700

    Create pointers12.c

---
+++ pointers12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void fun1() { printf("Fun1\n"); }
+void fun2() { printf("Fun2\n"); }
+void wrapper(void (*fun)())
+{
+fun();
+}
+int main()
+{
+wrapper(fun1);
+wrapper(fun2);
+return 0;
+}
+
+//Output: 
+//Fun1 
+//Fun2
